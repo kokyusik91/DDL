891a0de2f09df1a291098d6679b139752cb47b7c
kks91 <kokyusik91@naver.com>
2022-09-26 19:28:58 +0900

    🚧  Implement ButtonGroup Code

---
+++ src/ButtonGroup.stories.tsx
@@ -0,0 +1,74 @@
+import { ComponentStory, Meta } from '@storybook/react';
+
+import ButtonGroup from './ButtonGroups';
+import Button from './Button';
+
+export default {
+  title: 'stories/Button-Group',
+  component: ButtonGroup,
+  parameters: {
+    componentSubtitle: 'DGDR 서비스에서 사용하는 버튼그룹을 정의합니다.',
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof ButtonGroup>;
+
+const Template: ComponentStory<typeof ButtonGroup> = (args) => (
+  <ButtonGroup {...args}></ButtonGroup>
+);
+
+// basic 버튼 🥰
+export const Basic = Template.bind({});
+Basic.args = {
+  direction: 'row',
+  gap: '16px',
+  children: (
+    <>
+      <Button text="button" color="white" disabled={false} />
+      <Button text="button" color="green" disabled={false} />
+    </>
+  ),
+};
+
+// basic 버튼 🥰
+export const Green = Template.bind({});
+Green.args = {
+  direction: 'row',
+  gap: '16px',
+  children: (
+    <>
+      <Button text="button" color="white" disabled={false} />
+      <Button text="button" color="green" disabled={false} />
+    </>
+  ),
+};
+
+// basic 버튼 🥰
+export const Red = Template.bind({});
+Red.args = {
+  direction: 'row',
+  gap: '16px',
+  children: (
+    <>
+      <Button text="button" color="white" disabled={false} />
+      <Button text="button" color="red" disabled={false} />
+    </>
+  ),
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  direction: 'row',
+  gap: '16px',
+  children: (
+    <>
+      <Button text="button" color="white" disabled={false} />
+      <Button text="button" color="red" disabled={true} />
+    </>
+  ),
+};
--- src/ButtonGroups.tsx
+++ src/ButtonGroups.tsx
@@ -1,1 +1,44 @@
-export {}
+import styled from 'styled-components';
+
+interface ButtonGroupProps {
+  /** 버튼을 보여줄 방향 */
+  direction: 'row' | 'column';
+  /** 버튼을 우측에 보여줍니다. */
+  rightAlign?: boolean;
+  /** 버튼과 버튼사이의 간격을 설정합니다. */
+  gap: string;
+  /** 버튼 그룹에서 보여줄 버튼들 */
+  children: React.ReactNode;
+  /* 스타일 커스터마이징 하고싶을 때 사용 */
+  className?: string;
+}
+
+/**
+ * DDL의 기본 버튼 그룹 컴포넌트 입니다
+ *
+ * - `direction` 값을 `row`로 설정하면 버튼이 **가로 배치** 됩니다. 'column'로 설정하면 버튼이 **세로 배치** 됩니다. 기본값은 `row` 입니다.
+ * - `gap` 값을 `active`로 설정하면 버튼이 **활성화** 됩니다. 기본값은 `active` 입니다.
+ * - `onClick` 속성은 버튼을 클릭했을때 실행시킬 콜백함수를 넘길수 있습니다.
+ */
+
+export default function ButtonGroup({
+  direction = 'row',
+  gap = '16px',
+  children,
+}: ButtonGroupProps) {
+  return (
+    <DDLButtonGroup direction={direction} gap={gap}>
+      {children}
+    </DDLButtonGroup>
+  );
+}
+
+const DDLButtonGroup = styled.div<{ direction: string; gap: string }>`
+  width: 100%;
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+
+  button:first-child {
+    margin-right: ${(props) => props.gap};
+  }
+`;
