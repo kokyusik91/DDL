ac3a8ee7c81fd4ea0090007054a53bcd8c6ad4c3
kks91 <kokyusik91@naver.com>
2022-09-26 19:27:19 +0900

    🚧  Set storybook's config files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  dgdr: {
+    name: 'DGDR Main viewport',
+    styles: {
+      width: '360px',
+      height: '600px',
+    },
+  },
+};
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  viewport: {
+    viewports: { ...INITIAL_VIEWPORTS, ...customViewports }, // newViewports would be an ViewportMap. (see below for examples)
+    defaultViewport: 'someDefault',
+  },
+};
