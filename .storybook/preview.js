683fcf907b6397dc53a5357e6f8d2c390f6cf554
kks91 <kokyusik91@naver.com>
2022-09-28 21:54:35 +0900

    ⚡️ update decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/global-styles';
+import theme from '../styles/theme/index';
 
 const customViewports = {
   dgdr: {
@@ -23,3 +26,14 @@ export const parameters = {
     defaultViewport: 'someDefault',
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      {/* <StylesProvider injectFirst> */}
+      <GlobalStyle />
+      <Story />
+      {/* </StylesProvider> */}
+    </ThemeProvider>
+  ),
+];
