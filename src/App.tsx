133742908ee4175afed2742122e3d76615001dd6
kks91 <kokyusik91@naver.com>
2022-10-19 20:47:37 +0900

    💄  Add Button and Button group component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import Button from './Button';
+import ButtonGroup from './ButtonGroups';
+import Typo from './Typo';
 
 function App() {
   return (
     <>
       <Button text="보튼" disabled={false} color="red" />
+      <ButtonGroup gap="16px" direction="row">
+        <Button text="보튼" disabled={false} color="red" />
+        <Button text="보튼" disabled={false} color="green" />
+      </ButtonGroup>
+
+      <Typo type="h1">게시글</Typo>
     </>
   );
 }
