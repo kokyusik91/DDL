8615f1feacbef33179596480de90c702f8a7da66
kks91 <kokyusik91@naver.com>
2022-10-20 16:32:34 +0900

    ✨ Implement Color stories

---
+++ src/Color.stories.tsx
@@ -0,0 +1,71 @@
+import Color from './Color';
+import theme from './styles/theme';
+import styled from 'styled-components';
+
+export default {
+  title: 'stories/Color',
+  component: Color,
+};
+
+export const color = () => {
+  return (
+    <>
+      <Container>
+        <Color
+          colorValue={theme.palette.dgdrDeepGreen}
+          colorName="dgdr_deep_green"
+        />
+      </Container>
+      <Container>
+        <Color colorValue={theme.palette.main.green} colorName="dgdr_green" />
+        <Color
+          colorValue={theme.palette.main.orangeRed}
+          colorName="dgdr_orangered"
+        />
+        <Color colorValue={theme.palette.main.blue} colorName="dgdr_blue" />
+        <Color colorValue={theme.palette.main.purple} colorName="dgdr_purple" />
+        <Color colorValue={theme.palette.main.yellow} colorName="dgdr_yellow" />
+      </Container>
+      <Container>
+        <Color
+          colorValue={theme.palette.sub.green}
+          colorName="dgdr_sub_green"
+        />
+        <Color
+          colorValue={theme.palette.sub.orangeRed}
+          colorName="dgdr_sub_orangered"
+        />
+        <Color colorValue={theme.palette.sub.blue} colorName="dgdr_sub_blue" />
+        <Color
+          colorValue={theme.palette.sub.purple}
+          colorName="dgdr_sub_purple"
+        />
+        <Color
+          colorValue={theme.palette.sub.yellow}
+          colorName="dgdr_sub_yellow"
+        />
+      </Container>
+      <Container>
+        <Color colorValue={theme.palette.grey[100]} colorName="dgdr_100" />
+        <Color colorValue={theme.palette.grey[200]} colorName="dgdr_200" />
+        <Color colorValue={theme.palette.grey[300]} colorName="dgdr_300" />
+        <Color colorValue={theme.palette.grey[400]} colorName="dgdr_400" />
+        <Color colorValue={theme.palette.grey[500]} colorName="dgdr_500" />
+        <Color colorValue={theme.palette.grey[600]} colorName="dgdr_600" />
+        <Color colorValue={theme.palette.grey[700]} colorName="dgdr_700" />
+        <Color colorValue={theme.palette.grey[800]} colorName="dgdr_800" />
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.section`
+  display: flex;
+  margin-bottom: 20px;
+  margin-right: 20px;
+  flex-wrap: wrap;
+`;
+
+color.story = {
+  name: 'color',
+};
---
+++ src/Color.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import theme from './styles/theme';
+import palette from './styles/theme/palette';
+
+interface ColorProps {
+  colorValue: string;
+  colorName: string;
+}
+
+/**
+ *
+ * DDL의 color chip 입니다.
+ *
+ * - dgdr_deep_green : `#18806A`
+ * - dgdr_green : `#00D4AA`
+ * - dgdr_orangered : `#FE6A6A`
+ * - dgdr_blue : `#467FF7`
+ * - dgdr_purple : `#BB39FA`
+ * - dgdr_yellow : `#FCB707`
+ * - dgdr_sub_green : `#E6FBF7`
+ * - dgdr_sub_orangered : `#FFF0F0`
+ * - dgdr_sub_blue : `#EDF2FE`
+ * - dgdr_sub_purple : `#F2E5F8`
+ * - dgdr_sub_yellow : `#FFF5D6`
+ * - dgdr_100 : `#FFFFFF`
+ * - dgdr_200 : `#F8F8F8`
+ * - dgdr_300 : `#F0F0F0`
+ * - dgdr_400 : `#DFDFDF`
+ * - dgdr_500 : `#C9C9C9`
+ * - dgdr_600 : `#ABABAB`
+ * - dgdr_700 : `#797979`
+ * - dgdr_800 : `#2F3E39`
+ */
+
+export default function Color({ colorValue, colorName }: ColorProps) {
+  return (
+    <>
+      <ColorContainer>
+        <ColorChip colorCode={colorValue} />
+        <h1>{`${colorName}`}</h1>
+        <h2>{`${colorValue}`}</h2>
+      </ColorContainer>
+    </>
+  );
+}
+
+const ColorContainer = styled.article`
+  width: 144px;
+  border-radius: 6px;
+  padding: 8px;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.12);
+  margin-right: 20px;
+
+  h1 {
+    margin: 0;
+    ${theme.typography.h3}
+    color: ${palette.grey[800]};
+    margin-bottom: 8px;
+  }
+
+  h2 {
+    margin: 0;
+    ${theme.typography.sub1}
+    color: ${palette.grey[600]};
+  }
+`;
+
+const ColorChip = styled.div<{ colorCode: string }>`
+  width: 100%;
+  height: 64px;
+  border-radius: 4px;
+  background-color: ${(props) => props.colorCode};
+  margin-bottom: 8px;
+`;
