748a3c6f9243114307cb251ba1a81c56614e6286
kks91 <kokyusik91@naver.com>
2022-10-20 16:32:10 +0900

    ✨ Implement Style index.ts

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+import palette from './palette';
+import typography from './typography';
+// theme 저장
+const theme = {
+  palette,
+  typography,
+};
+export default theme;
