82e250631740c5726d84a59b710ea3a58464b6c3
kks91 <kokyusik91@naver.com>
2022-10-22 10:31:12 +0900

    🚧 Global setting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import React from 'react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../src/styles/global-styles';
+import theme from '../src/styles/theme';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 const customViewports = {
   dgdr: {
