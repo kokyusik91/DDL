2fdd65a5811711d09f4e044fe3d0edb23b4ebcec
kks91 <kokyusik91@naver.com>
2022-10-24 21:17:43 +0900

    ♻️ Refactor code

--- src/Component.tsx
+++ src/Component.tsx
@@ -1,12 +1,6 @@
 function Component() {
   return (
     <>
-      {/* <Button text="보튼" disabled={false} color="red" />
-      <ButtonGroup gap="16px" direction="row">
-        <Button text="보튼" disabled={false} color="red" />
-        <Button text="보튼" disabled={false} color="green" />
-      </ButtonGroup> */}
-
       <div></div>
     </>
   );
