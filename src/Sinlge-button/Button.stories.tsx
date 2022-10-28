b0cf19a79b2877c22b995817a697a5cf61f0bd29
kks91 <kokyusik91@naver.com>
2022-10-28 21:13:07 +0900

    🚧 change button component properties

--- src/Sinlge-button/Button.stories.tsx
+++ src/Sinlge-button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentStory, Meta } from '@storybook/react';
+import styled from 'styled-components';
 
 import Button from './Button';
 
@@ -10,44 +11,46 @@ export default {
   },
   decorators: [
     (Story) => (
-      <div style={{}}>
+      <StoryContainer>
         <Story />
-      </div>
+      </StoryContainer>
     ),
   ],
 } as Meta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-// basic 버튼 🥰
-export const Basic = Template.bind({});
-Basic.args = {
-  color: 'green',
-  disabled: false,
-  onClick: () => {
-    alert('기본 버튼 클릭!');
-  },
-};
-
 export const Green = Template.bind({});
 Green.args = {
   color: 'green',
   disabled: false,
+  fullWidth: false,
 };
 
 export const Red = Template.bind({});
 Red.args = {
   color: 'red',
   disabled: false,
+  fullWidth: false,
 };
 
 export const White = Template.bind({});
 White.args = {
   color: 'white',
   disabled: false,
+  fullWidth: false,
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   disabled: true,
+  fullWidth: false,
 };
+
+const StoryContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
