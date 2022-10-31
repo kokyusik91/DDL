b80e0110de02c75944892ebc9e995f96c66ba4b3
kks91 <kokyusik91@naver.com>
2022-10-31 10:57:24 +0900

    🚧 Implement ButtonGroup stories

--- src/advanced-button-group/ButtonGroup.stories.tsx
+++ src/advanced-button-group/ButtonGroup.stories.tsx
@@ -3,7 +3,7 @@ import ButtonGroup from './ButtonGroup';
 import Button from '../advanced-button/Button';
 
 export default {
-  title: 'button/Advanced-Button-group',
+  title: 'button/Multiple-button',
   component: ButtonGroup,
   parameter: {
     componentSubtitle: 'DGDR 서비스에서 사용하는 버튼 그룹을 정의합니다.',
@@ -28,7 +28,7 @@ Green.args = {
           alert('취소 버튼 클릭!');
         }}
       >
-        버튼
+        취소
       </Button>
       <Button
         buttonColor="Green"
@@ -37,7 +37,64 @@ Green.args = {
           alert('확인 버튼 클릭!');
         }}
       >
-        버튼
+        확인
+      </Button>
+    </>
+  ),
+};
+
+export const Red = Template.bind({});
+Red.args = {
+  gap: 16,
+  fullWidth: false,
+  children: (
+    <>
+      <Button
+        buttonColor="White"
+        fullWidth
+        onClick={() => {
+          alert('취소 버튼 클릭!');
+        }}
+      >
+        취소
+      </Button>
+      <Button
+        buttonColor="Red"
+        fullWidth
+        onClick={() => {
+          alert('확인 버튼 클릭!');
+        }}
+      >
+        확인
+      </Button>
+    </>
+  ),
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  gap: 16,
+  fullWidth: false,
+  children: (
+    <>
+      <Button
+        buttonColor="White"
+        fullWidth
+        onClick={() => {
+          alert('취소 버튼 클릭!');
+        }}
+      >
+        취소
+      </Button>
+      <Button
+        buttonColor="Red"
+        fullWidth
+        disabled
+        onClick={() => {
+          alert('확인 버튼 클릭!');
+        }}
+      >
+        확인
       </Button>
     </>
   ),
