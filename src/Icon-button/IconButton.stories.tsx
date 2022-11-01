1cf52e26bed03c85fb5f325035b302b2e444667e
kks91 <kokyusik91@naver.com>
2022-11-01 17:10:07 +0900

    ✨ Implement IconButton Component

--- src/Icon-button/IconButton.stories.tsx
+++ src/Icon-button/IconButton.stories.tsx
@@ -1,12 +1,13 @@
 import { ComponentStory, Meta } from '@storybook/react';
+import styled from 'styled-components';
+import PopUp from '../pop-up/PopUp';
 import IconButton from './IconButton';
-import Icon from '../dgdr-Icon/Icon';
 
 export default {
   title: 'button/Icon-button',
   component: IconButton,
   parameters: {
-    componentSubtitle: 'DGDR 서비스에서 사용하는 아이콘 버튼을 정의 합니다',
+    componentSubtitle: 'DGDR 서비스에서 사용하는 Modal 버튼을 정의 합니다.',
   },
 } as Meta<typeof IconButton>;
 
@@ -14,9 +15,52 @@ const Template: ComponentStory<typeof IconButton> = (args) => {
   return <IconButton {...args} />;
 };
 
-export const Basic = Template.bind({});
-Basic.args = {
-  text: 'button',
-  color: 'green',
-  icon: <Icon icon="close_small" />,
+export const Green = Template.bind({});
+Green.args = {
+  children: 'button',
+  buttonColor: 'Green',
+  endIcon: 'plus_round_small',
 };
+
+export const Red = Template.bind({});
+Red.args = {
+  children: 'button',
+  buttonColor: 'Red',
+  endIcon: 'close_round_small',
+};
+
+// 디자이너에게 보여주는 템플릿
+const DesignTemplate: ComponentStory<typeof PopUp> = () => {
+  return (
+    <DesginTemplateContainer>
+      <li>
+        <IconButton
+          buttonColor="Green"
+          endIcon="plus_round_small"
+          fullWidth={false}
+        >
+          button
+        </IconButton>
+      </li>
+      <li>
+        <IconButton
+          buttonColor="Red"
+          endIcon="close_round_small"
+          fullWidth={false}
+        >
+          button
+        </IconButton>
+      </li>
+    </DesginTemplateContainer>
+  );
+};
+
+export const DesignQATemplate = DesignTemplate.bind({});
+
+const DesginTemplateContainer = styled.ul`
+  display: inline-flex;
+  flex-direction: column;
+  li {
+    margin-bottom: 20px;
+  }
+`;
--- src/Icon-button/IconButton.tsx
+++ src/Icon-button/IconButton.tsx
@@ -1,23 +1,74 @@
-import { ReactNode } from 'react';
 import styled from 'styled-components';
+import theme from '../styles/theme';
+import { assginText } from '../advanced-button/Button';
+import { TextType } from '../Typography/Typo';
+import { default as Icon, IconType } from '../dgdr-Icon/Icon';
 
 export type IconButtonProps = {
-  icon: ReactNode;
-  color: string;
-  text: string;
+  /** Icon-button에 들어갈 텍스트 입니다. */
+  children: string;
+  /** Icon-button의 색상 입니다. */
+  buttonColor: 'Green' | 'Red';
+  /** Icon-button의 너비를 나타냅니다. */
+  fullWidth: boolean;
+  /** Icon-button에 들어갈 텍스트 사이즈 입니다. */
+  textType?: TextType;
+  /** Icon-button에 들어갈 아이콘 타입 입니다. */
+  endIcon: IconType;
 };
 
-function IconButton({ icon, color, text }: IconButtonProps) {
+function assignIconButtonColor(colorType: 'Green' | 'Red') {
+  const colorChip = {
+    Green: {
+      backgroundColor: theme.palette.sub.green,
+      textColor: theme.palette.main.green,
+    },
+    Red: {
+      backgroundColor: theme.palette.sub.orangeRed,
+      textColor: theme.palette.main.orangeRed,
+    },
+  };
+  return colorChip[colorType];
+}
+
+function IconButton({
+  children,
+  buttonColor,
+  fullWidth = false,
+  textType = 'body1_M',
+  endIcon = 'plus_round_small',
+}: IconButtonProps) {
   return (
-    <IconButtonWrapper color={color}>
-      {text}
-      {icon}
+    <IconButtonWrapper
+      buttonColor={buttonColor}
+      fullWidth={fullWidth}
+      textType={textType}
+    >
+      {children}
+      <Icon
+        icon={endIcon}
+        color={assignIconButtonColor(buttonColor).textColor}
+        customStyle={{ marginLeft: '4px' }}
+      />
     </IconButtonWrapper>
   );
 }
 
-const IconButtonWrapper = styled.button<{ color: string }>`
-  background-color: ${(props) => props.color};
+const IconButtonWrapper = styled.button<{
+  buttonColor: 'Green' | 'Red';
+  fullWidth: boolean;
+  textType: TextType;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => (props.fullWidth ? '100%' : '328px')};
+  padding: 16px 0;
+  border-radius: 14px;
+  background-color: ${(props) =>
+    assignIconButtonColor(props.buttonColor).backgroundColor};
+  ${(props) => assginText(props.textType)};
+  color: ${(props) => assignIconButtonColor(props.buttonColor).textColor};
 `;
 
 export default IconButton;
