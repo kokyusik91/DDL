dd4c53c1efde0da77319f378f5e41b58ce7cd797
kks91 <kokyusik91@naver.com>
2022-11-09 18:20:09 +0900

    ✨ Implement TextField Component and that's stories

---
+++ src/textField/TextField.stories.tsx
@@ -0,0 +1,106 @@
+import { ComponentStory, Meta } from '@storybook/react';
+import TextInput from './TextInput';
+import BasicInput from '../input/BasicInput';
+import styled from 'styled-components';
+import InputLabel from '../input-label/InputLabel';
+
+export default {
+  title: 'Textfield/Basic-Textfield',
+  component: TextInput,
+  parameters: {
+    componentSubtitle: 'DGDR 서비스에서 사용하는 Text Field을 정의 합니다.',
+  },
+} as Meta<typeof TextInput>;
+
+const Template: ComponentStory<typeof TextInput> = (args) => {
+  return <TextInput {...args} />;
+};
+
+export const Basic = Template.bind({});
+Basic.args = {
+  children: (
+    <>
+      <BasicInput placeholder="welcome@dgdr.co.kr" />
+    </>
+  ),
+};
+
+export const label = Template.bind({});
+label.args = {
+  children: (
+    <>
+      <BasicInput
+        placeholder="welcome@dgdr.co.kr"
+        label={<InputLabel essential htmlFor="인풋" text="레이블" />}
+      />
+    </>
+  ),
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  status: 'isSucess',
+  children: (
+    <>
+      <BasicInput status="isSucess" placeholder="welcome@dgdr.co.kr" />
+    </>
+  ),
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  status: 'isError',
+  children: (
+    <>
+      <BasicInput status="isError" placeholder="welcome@dgdr.co.kr" />
+    </>
+  ),
+  errorMsg: 'error message',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: (
+    <>
+      <BasicInput placeholder="welcome@dgdr.co.kr" disabled />
+    </>
+  ),
+};
+
+// 디자이너에게 보여주는 템플릿
+const DesignTemplate: ComponentStory<typeof TextInput> = () => {
+  return (
+    <DesginTemplateContainer>
+      <li>
+        <TextInput>
+          <BasicInput placeholder="welcome@dgdr.co.kr" />
+        </TextInput>
+      </li>
+      <li>
+        <TextInput status="isSucess">
+          <BasicInput status="isSucess" placeholder="welcome@dgdr.co.kr" />
+        </TextInput>
+      </li>
+      <li>
+        <TextInput status="isError" errorMsg="error message">
+          <BasicInput status="isError" placeholder="welcome@dgdr.co.kr" />
+        </TextInput>
+      </li>
+      <li>
+        <TextInput>
+          <BasicInput placeholder="welcome@dgdr.co.kr" disabled />
+        </TextInput>
+      </li>
+    </DesginTemplateContainer>
+  );
+};
+
+export const DesignQATemplate = DesignTemplate.bind({});
+
+const DesginTemplateContainer = styled.ul`
+  display: inline-flex;
+  flex-direction: column;
+  li {
+    margin-bottom: 20px;
+  }
+`;
--- src/textField/TextInput.tsx
+++ src/textField/TextInput.tsx
@@ -1,34 +1,67 @@
 import { ReactNode } from 'react';
 import styled from 'styled-components';
+import { useState } from 'react';
+import Icon from '../dgdr-Icon/Icon';
+import theme from '../styles/theme';
+import InputLabel from '../input-label/InputLabel';
 
 interface TextInputProps {
-  /** Input text 입니다. */
-  value: string;
   /** Input의 너비 입니다. */
-  fullWidth: boolean;
+  fullWidth?: boolean;
   /** Input에 들어갈 아이콘 컴포넌트 입니다 */
   icon?: ReactNode;
   /** Input에 들어갈 placeholder 입니다. */
   placeholder?: string;
+  /** Input의 현재 상태입니다. */
+  status?: 'isSucess' | 'isError';
+  /** Input의 에러 메세지 입니다. */
+  errorMsg?: string;
+  /** Input 컴포넌트 입니다. */
+  children: ReactNode;
 }
 
 export default function TextInput({
-  value,
-  fullWidth,
-  icon,
-  placeholder,
+  fullWidth = false,
+  status,
+  errorMsg,
+  children,
 }: TextInputProps) {
   return (
-    <CustomInput
-      fullWidth={fullWidth}
-      value={value}
-      placeholder={placeholder}
-    />
+    <>
+      <InputContainer fullWidth={fullWidth}>
+        {children}
+        {status && (
+          <Icon
+            icon={status === 'isSucess' ? 'check' : 'close_round_small'}
+            color={
+              status === 'isSucess'
+                ? theme.palette.main.green
+                : theme.palette.grey[500]
+            }
+          />
+        )}
+      </InputContainer>
+      {status === 'isError' && <ErrorMessage>{errorMsg}</ErrorMessage>}
+    </>
   );
 }
 
-const CustomInput = styled.input<{ fullWidth: boolean }>`
+const InputContainer = styled.div<{ fullWidth: boolean }>`
+  position: relative;
   width: ${(props) => (props.fullWidth ? '100%' : '328px')};
-  border: 1px solid ${({ theme }) => theme.palette.grey[300]};
-  border-radius: 14px;
+
+  svg {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const ErrorMessage = styled.strong`
+  margin-left: 16px;
+  margin-top: 8px;
+  display: block;
+  ${({ theme }) => theme.typography.body3};
+  color: ${({ theme }) => theme.palette.main.orangeRed};
 `;
