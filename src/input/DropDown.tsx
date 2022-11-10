15e1463c539a7ae04494559ccb9d787cd9e1e0fe
kks91 <kokyusik91@naver.com>
2022-11-10 11:12:18 +0900

    🚧 is implementing DropDown component

--- src/input/DropDown.tsx
+++ src/input/DropDown.tsx
@@ -1,19 +1,36 @@
 import styled from 'styled-components';
+import Icon, { IconType } from '../dgdr-Icon/Icon';
 
 type DropDownProps = {
   title: string;
+  icon: IconType;
+  fullWidth?: boolean;
 };
 
-function DropDown({ title }: DropDownProps) {
+function DropDown({ title, icon, fullWidth = false }: DropDownProps) {
   return (
-    <DropDownContainer>
+    <DropDownContainer fullWidth={fullWidth}>
       <h1>{title}</h1>
+      <Icon icon={icon} />
     </DropDownContainer>
   );
 }
 
-const DropDownContainer = styled.div`
+const DropDownContainer = styled.div<{ fullWidth?: boolean }>`
+  display: relative;
+  width: ${(props) => (props.fullWidth ? '100%' : '328px')};
+  padding: 16px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 14px;
   background-color: ${({ theme }) => theme.palette.grey[100]};
+
+  & > svg {
+    position: absolute;
+    right: 0;
+    top: 50%;
+  }
 
   & > h1 {
     color: ${({ theme }) => theme.palette.grey[800]};
